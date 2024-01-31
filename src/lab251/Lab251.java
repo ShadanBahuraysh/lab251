c73fac80faa5685357b11b1f10ec1fa8707ebc73
Shadan <ishadan.ab@gmail.com>
2024-01-31 14:50:25 +0300

    print statment

---
+++ src/lab251/Lab251.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab251;
+
+/**
+ *
+ * @author 2206554
+ */
+public class Lab251 {
+
+    public static void main(String[] args) {
+      System.out.println("hello");
+    }
+    
+}
